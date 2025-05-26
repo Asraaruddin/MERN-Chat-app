0958e3708713fcce60543eb8d6e235973177bf6c
AsrarUddin <asraruddin000@gmail.com>
2025-05-26 12:16:11 +0530

    Setup JWT authentication and bcrypt password hashing

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+dotenv.config(); 
 const cors = require("cors");        // require cors at top
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const userRoutes = require('./routes/userRoutes')
 
+
+
+
+connectDB();
 const app = express();
-dotenv.config();
+
+app.use(express.json()) //to  accept JSON Data 
 
 app.use(cors());                     // use cors BEFORE routes
 
@@ -12,14 +20,7 @@ app.get("/", (req, res) => {
   res.send("API is running successfully ");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use('/api/user',userRoutes)
 
 const PORT = process.env.PORT || 5000;
 
