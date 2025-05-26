210df241fb7d7beb7e299a21ed4ae87f7b26c63c
AsrarUddin <asraruddin000@gmail.com>
2025-05-26 18:02:46 +0530

    Add custom error-handling middleware

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,8 @@ dotenv.config();
 const cors = require("cors");        // require cors at top
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
-const userRoutes = require('./routes/userRoutes')
-
-
+const userRoutes = require('./routes/userRoutes');
+const {notFound,errorHandler} = require('./middleware/errorMiddleware')
 
 
 connectDB();
@@ -20,7 +19,9 @@ app.get("/", (req, res) => {
   res.send("API is running successfully ");
 });
 
-app.use('/api/user',userRoutes)
+app.use('/api/user',userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
