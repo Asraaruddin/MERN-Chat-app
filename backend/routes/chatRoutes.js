c61b47646cdab4a205b6ffd92ae4f0d301a4b766
AsrarUddin <asraruddin000@gmail.com>
2025-05-27 23:08:51 +0530

    updated routes

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { protect } = require("../middleware/AuthMiddleWare");
+const {accessChat} = require("../controllers/chatController")
 const router =  express.Router();
 
 router.route("/").post(protect,accessChat);
 router.route("/").get(protect,fetchChats);
 router.route("/group").get(protect,createGroupChat);
 router.route("/rename").put(protect,renameGroup);
-router.route("/groupremove").put(protect,renameGroup);
 router.route("/groupradd").put(protect,addToGroup);
+router.route("/groupremove").put(protect,renameGroup);
+
 
 module.exports = router;
