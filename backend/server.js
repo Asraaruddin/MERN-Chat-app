cd1223171b439c2ef5029f54d12b40fded18a583
AsrarUddin <asraruddin000@gmail.com>
2025-05-31 23:37:30 +0530

    socket.io setup

--- backend/server.js
+++ backend/server.js
@@ -29,4 +29,36 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
+const server  =  app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
+
+const io = require('socket.io')(server,{
+  pingTimeout:60000,
+  cors:{
+    origin:"http://localhost:3000",
+  },
+})
+io.on("connection",(socket)=>{
+  console.log("connected to socket.io");
+  socket.on('setup',(userData)=>{
+    socket.join(userData._id);
+    socket.emit('connected');
+  });
+  socket.on('join chat',(room)=>{
+    socket.join(room);
+    console.log("User JOINED Room:" +room);
+  });
+  socket.on("new message",(newMessageRecieved)=>{
+    var chat = newMessageRecieved.chat;
+
+    if(!chat.users) return console.log("chat.users not defined");
+    chat.users.forEach(user=>{
+      if(user._id===newMessageRecieved.send._id) return;
+
+      socket.in(user._id).emit("message recieved",newMessageRecieved);
+    })
+  });
+  socket.off("setup",()=>{
+    console.log("User disconnected");
+    socket.leave(userData._id)
+  })
+})
