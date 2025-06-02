06d0a2076be55c3a50520816073297469a567973
AsrarUddin <asraruddin000@gmail.com>
2025-06-02 15:45:14 +0530

    Add log to server.js to test deployment

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,8 @@ const PORT = process.env.PORT || 5000;
 
 const server  =  app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
 
+
+
 const io = require('socket.io')(server,{
   pingTimeout:60000,
   cors:{
